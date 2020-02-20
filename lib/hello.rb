5092819e3d88b29e359d1e069277308fabc2392c
Simo Khouili <m.simokhouili@gmail.com>
2020-02-20 01:24:48 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,7 +8,7 @@ while i < array.length
   end
 end
 
-hello_t["Tim", "Tom", "Jim"]
+# hello_t["Tim", "Tom", "Jim"]
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
