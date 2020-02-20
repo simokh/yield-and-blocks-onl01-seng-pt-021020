ba105f7ff7b30c452edabcf28599bcf7cac7df98
Simo Khouili <m.simokhouili@gmail.com>
2020-02-20 01:27:42 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+  if block_given?
 
 i = 0
 
@@ -7,6 +8,9 @@ while i < array.length
   i = i +1
   end
   array
+else
+  puts "Hey! No block was given!"
+ end
 end
 
 # hello_t["Tim", "Tom", "Jim"]
